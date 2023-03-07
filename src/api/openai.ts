3f90bd73ff9d2ac87e017f2ca60c4d0529a52313
Fumaz <mail@fumaz.dev>
2023-03-07 20:47:48 +0100

    mh

--- src/api/openai.ts
+++ src/api/openai.ts
@@ -40,7 +40,7 @@ export async function generateImage(prompt: string) {
     const completion = await openai.createImage({
         prompt: prompt,
         n: 1,
-        size: "1024x1024",
+        size: "256x256",
         response_format: "url"
     });
 
